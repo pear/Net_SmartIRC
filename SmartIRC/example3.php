17775064a1af0682526870f5339e4ca8b6231d0e
Mirco Bauer <meebey@php.net>
2003-01-06 00:42:04 +0000

    first import (new example)
    git-svn-id: https://svn.php.net/repository/pear/packages/Net_SmartIRC/trunk@110644 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ SmartIRC/example3.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * $Id$
+ * $Revision$
+ * $Author$
+ * $Date$
+ */
+/**
+ * Copyright (C) 2002-2003 Mirco "MEEBEY" Bauer <mail@meebey.net> <http://www.meebey.net>
+ * 
+ * Full LGPL License: <http://www.meebey.net/lgpl.txt>
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+// ---EXAMPLE OF HOW TO USE Net_SmartIRC---
+// this code shows how a mini php bot could be written
+include_once('../SmartIRC.php');
+
+class mybot
+{
+    function op_list(&$irc, &$data)
+    {
+        $irc->message(SMARTIRC_TYPE_CHANNEL, '#smartirc-test', 'ops on this channel are:');
+        
+        $oplist = '';
+        foreach ($irc->channel['#test']->ops as $key => $value) {
+            $oplist .= ' '.$key;
+        }
+        
+        // result is send to #smartirc-test (we don't want to spam #test)
+        $irc->message(SMARTIRC_TYPE_CHANNEL, '#smartirc-test', $oplist);
+    }
+}
+
+$bot = &new mybot();
+$irc = &new Net_SmartIRC();
+$irc->setDebug(SMARTIRC_DEBUG_ALL);
+$irc->setUseSockets(TRUE);
+$irc->setChannelSynching(TRUE);
+$irc->registerActionhandler(SMARTIRC_TYPE_CHANNEL, '^!ops', $bot, 'op_list');
+$irc->connect('irc.freenet.de', 6667);
+$irc->login('Net_SmartIRC', 'Net_SmartIRC Client '.SMARTIRC_VERSION, 8, 'Net_SmartIRC');
+$irc->join('#smartirc-test');
+$irc->join('#test');
+$irc->listen();
+$irc->disconnect();
+?>
