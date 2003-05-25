ef5402b69e31d203cbe005b065f75f7e64ef5fed
Mirco Bauer <meebey@php.net>
2003-05-25 16:40:42 +0000

    - removed not needed SMARTIRC_ prefix for debug output.
    git-svn-id: https://svn.php.net/repository/pear/packages/Net_SmartIRC/trunk@128715 c90b9560-bf6c-de11-be94-00142212c4b1

--- SmartIRC.php
+++ SmartIRC.php
@@ -1603,68 +1603,68 @@ class Net_SmartIRC
     {
         $filename = $this->_modulepath.'/'.$name.'.php';
         if (!file_exists($filename)) {
-            $this->log(SMARTIRC_DEBUG_MODULES, 'SMARTIRC_DEBUG_MODULES: couldn\'t load module "'.$filename.'" file doesn\'t exist');
+            $this->log(SMARTIRC_DEBUG_MODULES, 'DEBUG_MODULES: couldn\'t load module "'.$filename.'" file doesn\'t exist');
             return false;
         }
         
-        $this->log(SMARTIRC_DEBUG_MODULES, 'SMARTIRC_DEBUG_MODULES: loading module: "'.$name.'"...');
+        $this->log(SMARTIRC_DEBUG_MODULES, 'DEBUG_MODULES: loading module: "'.$name.'"...');
         // pray that there is no parse error, it will kill us!
         include_once($filename);
         $classname = 'Net_SmartIRC_module_'.$name;
         
         if (!class_exists($classname)) {
-            $this->log(SMARTIRC_DEBUG_MODULES, 'SMARTIRC_DEBUG_MODULES: class '.$classname.' not found in '.$filename);
+            $this->log(SMARTIRC_DEBUG_MODULES, 'DEBUG_MODULES: class '.$classname.' not found in '.$filename);
             return false;
         }
         
         $methods = get_class_methods($classname);
         if (!in_array('module_init', $methods)) {
-            $this->log(SMARTIRC_DEBUG_MODULES, 'SMARTIRC_DEBUG_MODULES: required method'.$classname.'::module_init not found, aborting...');
+            $this->log(SMARTIRC_DEBUG_MODULES, 'DEBUG_MODULES: required method'.$classname.'::module_init not found, aborting...');
             return false;
         }
         
         if (!in_array('module_exit', $methods)) {
-            $this->log(SMARTIRC_DEBUG_MODULES, 'SMARTIRC_DEBUG_MODULES: required method'.$classname.'::module_exit not found, aborting...');
+            $this->log(SMARTIRC_DEBUG_MODULES, 'DEBUG_MODULES: required method'.$classname.'::module_exit not found, aborting...');
             return false;
         }
         
         $vars = array_keys(get_class_vars($classname));
         if (!in_array('name', $vars)) {
-            $this->log(SMARTIRC_DEBUG_MODULES, 'SMARTIRC_DEBUG_MODULES: required variable '.$classname.'::name not found, aborting...');
+            $this->log(SMARTIRC_DEBUG_MODULES, 'DEBUG_MODULES: required variable '.$classname.'::name not found, aborting...');
             return false;
         }
         
         if (!in_array('description', $vars)) {
-            $this->log(SMARTIRC_DEBUG_MODULES, 'SMARTIRC_DEBUG_MODULES: required variable '.$classname.'::description not found, aborting...');
+            $this->log(SMARTIRC_DEBUG_MODULES, 'DEBUG_MODULES: required variable '.$classname.'::description not found, aborting...');
             return false;
         }
         
         if (!in_array('author', $vars)) {
-            $this->log(SMARTIRC_DEBUG_MODULES, 'SMARTIRC_DEBUG_MODULES: required variable '.$classname.'::author not found, aborting...');
+            $this->log(SMARTIRC_DEBUG_MODULES, 'DEBUG_MODULES: required variable '.$classname.'::author not found, aborting...');
             return false;
         }
         
         if (!in_array('license', $vars)) {
-            $this->log(SMARTIRC_DEBUG_MODULES, 'SMARTIRC_DEBUG_MODULES: required variable '.$classname.'::license not found, aborting...');
+            $this->log(SMARTIRC_DEBUG_MODULES, 'DEBUG_MODULES: required variable '.$classname.'::license not found, aborting...');
             return false;
         }
         
         // looks like the module satisfies us
         $module = &new $classname;
-        $this->log(SMARTIRC_DEBUG_MODULES, 'SMARTIRC_DEBUG_MODULES: successful created instance of: '.$classname);
+        $this->log(SMARTIRC_DEBUG_MODULES, 'DEBUG_MODULES: successful created instance of: '.$classname);
         
-        $this->log(SMARTIRC_DEBUG_MODULES, 'SMARTIRC_DEBUG_MODULES: calling '.$classname.'::module_init()');
+        $this->log(SMARTIRC_DEBUG_MODULES, 'DEBUG_MODULES: calling '.$classname.'::module_init()');
         $module->module_init($this);
         $this->_modules[] = &$module;
         
-        $this->log(SMARTIRC_DEBUG_MODULES, 'SMARTIRC_DEBUG_MODULES: successful loaded module: '.$name);
+        $this->log(SMARTIRC_DEBUG_MODULES, 'DEBUG_MODULES: successful loaded module: '.$name);
         return true;
     }
     
    // experimentel feature, do not use it yet! 
     function unloadModule($name)
     {
-        $this->log(SMARTIRC_DEBUG_MODULES, 'SMARTIRC_DEBUG_MODULES: unloading module: '.$name.'...');
+        $this->log(SMARTIRC_DEBUG_MODULES, 'DEBUG_MODULES: unloading module: '.$name.'...');
 
         $modulecount = count($this->_modules);
         for ($i=0; $i < $modulecount; $i++) {
@@ -1675,12 +1675,12 @@ class Net_SmartIRC
                 $module->module_exit($this);
                 unset($this->_modules[$i]);
                 $this->_reordermodules();
-                $this->log(SMARTIRC_DEBUG_MODULES, 'SMARTIRC_DEBUG_MODULES: successful unloaded module: '.$name);
+                $this->log(SMARTIRC_DEBUG_MODULES, 'DEBUG_MODULES: successful unloaded module: '.$name);
                 return true;
             }
         }
         
-        $this->log(SMARTIRC_DEBUG_MODULES, 'SMARTIRC_DEBUG_MODULES: couldn\'t unloaded module: '.$name.' (it\'s not loaded!)');
+        $this->log(SMARTIRC_DEBUG_MODULES, 'DEBUG_MODULES: couldn\'t unloaded module: '.$name.' (it\'s not loaded!)');
         return false;
     }
     
